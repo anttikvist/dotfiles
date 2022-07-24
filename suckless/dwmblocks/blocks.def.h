05723fff32d278a8163cff8a439787691e6bb990
anttikvist <48825908+anttikvist@users.noreply.github.com>
2022-07-24 19:12:18 +0300

    updating stuff

--- suckless/dwmblocks/blocks.def.h
+++ suckless/dwmblocks/blocks.def.h
@@ -2,11 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/				/*Update Interval*/	/*Update Signal*/
 /*	{"Oulu: ",	"~/.scripts/weather",			600,			0}, */
-	{"",		"~/.scripts/updates",			600,			0},
-	{"",		"~/.scripts/memory",			5,			0},
-	{"",		"~/.scripts/disk",			30,			0},
+/*	{"",		"~/.scripts/updates",			600,			0}, */
+	{"",		"~/.scripts/memory",			5,			0}, 
+	{"",		"~/.scripts/disk",			30,			0}, 
 	{"",		"~/.scripts/battery",			10,			0},
-	{"", 		"~/.scripts/volume",			0,			10},	
+	{"", 		"~/.scripts/volume",			0,			10}, 
 	{"", 		"date '+%a %d. %Bta %H:%M'",		5,			0},
 };
 
